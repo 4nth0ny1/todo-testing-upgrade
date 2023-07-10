e8bc6326de0967479e2a835b7d8c2ef663a85a4c
4nth0ny1 <acatullo4@gmail.com>
2023-07-10 11:39:06 -0500

    moved tests out of components

--- src/tests/CreateTodo.test.jsx
+++ src/tests/CreateTodo.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import CreateTodo from "../ui/CreateTodo";
+import CreateTodo from "../components/todo/CreateTodo";
 
 describe("<CreateTodo Tests />", () => {
   test("it should mount", () => {
